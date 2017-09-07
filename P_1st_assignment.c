95142d3a6e236dd9baa51265bb92fc0c38b2b0b0
haavill <31677317+haavill@users.noreply.github.com>
2017-09-07 12:24:03 +0900

    P_1st_assignment
    프로그래밍연습2

---
+++ P_1st_assignment.c
@@ -0,0 +1,11 @@
+/* P_1st_assignment */
+#include <stdio.h>
+int main()
+{
+	printf("1. 2017301074\n"); //student id number
+	printf("2. Á¶ÇÏ¿¬ Hayeon Cho\n"); //name
+	printf("3. 010-3876-3272\n"); //mobile phone number
+	printf("4. github.com/haavill\n"); //GitHub address
+
+	return 0;
+}
